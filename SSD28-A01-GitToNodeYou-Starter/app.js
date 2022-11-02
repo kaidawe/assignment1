adc46d8ec2df4f27abe4cfc05b3b37c1a45e720a
Haley Dawe <hdawe@my.bcit.ca>
2022-11-02 15:44:41 -0700

    Updated app.js

--- SSD28-A01-GitToNodeYou-Starter/app.js
+++ SSD28-A01-GitToNodeYou-Starter/app.js
@@ -7,31 +7,51 @@
 
 // Set strict mode
 "use strict";
+const http = require("http");
 
-// * Load the core HTTP module so that we can create a server
+const hostname = "127.0.0.1";
+const port = 3000;
 
-// * Load the file helper functions with object destructuring from utils
+const {loadProfile, loadStatic} = require("./utils/fileHelper");
 
-// hostname and port are needed in order for the http server to listen for requests
-// * declare variables for these using 127.0.0.1 for hostname
-
-// * process.env.PORT will be defined by some hosts.  If undefined, use 3000.
-
-// Initialize our server
 const server = http.createServer((req, res) => {
   // branch based on the URL of the request
   switch (req.url) {
-    // Home page
-    // * Add a case that responds to / which sends "Hello Node Server" with a 200
+  
+    case("/"):
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Hello Node Server");
+      break;
 
     // Profiles Listing Page
     // * Add a case that responds to /profiles which sends "Profiles List" with a 200
 
     //   Individual Profile
-    case "/profiles/josh":
-      console.log("Josh is here.  Loading profile...");
+    case"/profiles":
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Profiles");
+      break;
+
+    case"/profiles/haley":
+      res.statusCode = 200;
+      console.log("Haley is here.  Loading profile...");
       loadProfile(req, res);
       break;
+    
+    case"/profiles/sam":
+    res.statusCode = 200;
+      console.log("Sam is here.  Loading profile...");
+      loadProfile(req, res);
+      break;
+
+    case"/profiles/yuko":
+    res.statusCode = 200;
+      console.log("Yuko is here.  Loading profile...");
+      loadProfile(req, res);
+      break;
+
 
     /* Add in a cases pointing at your personal profiles below */
 
@@ -43,11 +63,16 @@ const server = http.createServer((req, res) => {
       if (validStaticTypes.includes(pathSegments[3])) {
         loadStatic(req, res);
       } else {
-        // * set statusCode to 404
-        // * use res.setHeader to specify "Content-Type", "text/html"
-        // * send "File not found"
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("File Not Find");
+
       }
   }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 // * Set the HTTP server to listen on port, hostname as declared above
